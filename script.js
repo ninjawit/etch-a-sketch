b8f499b487156809ed22fe3070fe6ad0213d8052
ninjawit <80984772+ninjawit@users.noreply.github.com>
2023-05-11 14:47:29 -0500

    Add removeGrid function that clears grid-div between uses

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 const gridDiv = document.querySelector('#grid-div');
-
 const btn = document.querySelector('#size');
 
 btn.addEventListener('click', () => {
     let size = prompt("Enter a number between 1-100");
     if (size >= 1 && size <= 100) {
+        removeGrid();
         createGrid(size);
     }
-})
+    else {
+        alert("Please enter a valid number.");
+    }
+});
 
 function createGrid(gridSize) {
     gridDiv.style.setProperty('--columns-rows', gridSize);
@@ -21,4 +24,8 @@ function createGrid(gridSize) {
             div.style.backgroundColor = 'black';
         });
     }
+}
+
+function removeGrid() {
+    gridDiv.innerHTML = '';
 }
